811d050df3e62216d33aab454665cbf3866bbe88
hiren14 <hiren14lalni@gmail.com>
2023-08-07 19:25:32 +0530

    sanity error

--- dist/static/index-6ea4c5fb-244566dd.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-66ec8894-13a7b3d0.js";import"./desk-eb7b3548-d80aff6a.js";import"./json-inspector-8c3da3ef.js";export{m as DocumentPaneProvider,a as default};
